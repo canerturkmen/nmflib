e7a67f935870b0999aac213a12addce083e4ca86
Caner Turkmen <turkmen.ac@gmail.com>
2014-10-21 21:00:42 +0300

    added USPS digits, PNMF stable

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Welcome to
 NMFLIB is the early version of a lightweight library including different implementations of Non-negative
 Matrix Factorization, built using **python** and numpy.
 
-For the demo, in `demo.py`, we use the Wine and Multiple Features data sets from the UCI Repository, as well as
-Lenna included in SciPy.
+For the demo, in `demo.py`, we use the Wine data set from the UCI Repository, and the USPS handwritten digits data set
+as well as Lenna included in SciPy.
 
 For any questions you can always [email](mailto:turkmen.ac@gmail.com) me.
---
+++ demo_usps.py
@@ -0,0 +1,45 @@
+"""
+Projective NMF demo with the usps data set (handwritten digits)
+"""
+
+from nmf import NMF
+from projective import ProjectiveNMF
+from scipy import misc
+import pylab as pl
+import numpy as np
+from sklearn.metrics import normalized_mutual_info_score
+
+def usps(path="/Users/Caner/code/nmflib/data/usps/"):
+
+    train = np.loadtxt(path+"zip.train")
+    test = np.loadtxt(path+"zip.test")
+    A = np.vstack((train,test))
+
+    return A[:,0].astype('int'), A[:, 1:]
+
+def show_digit(arr):
+    arr_n = np.reshape(arr, (16,16))
+    pl.imshow(arr_n, cmap="gray")
+#%%
+
+labels, data = usps()
+
+data = data + np.ones(np.shape(data))
+
+#%%
+pnmf = ProjectiveNMF(data, 10, maxiter=500)
+
+result = pnmf.predict()
+
+pl.plot(result.convgraph)
+
+#%%
+
+w = result.matrices[0]
+#pl.imshow(w)
+
+assigned_labels = np.array(np.argmax(w, axis=1))[:,0]
+
+#%% Calculate NMI
+
+normalized_mutual_info_score(assigned_labels, labels)
