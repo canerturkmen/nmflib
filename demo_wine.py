62d8c7dda22ec32c9e0b5b135468c80aea7b7632
Caner Turkmen <turkmen.ac@gmail.com>
2014-10-21 18:24:56 +0300

    wine demo now achieves same NMI as MATLAB lib

--- demo_wine.py
+++ demo_wine.py
@@ -9,7 +9,7 @@ from projective import ProjectiveNMF
 from scipy import misc
 import pylab as pl
 import numpy as np
-
+from sklearn.metrics import normalized_mutual_info_score
 
 def wine(path="/Users/Caner/code/nmflib/data/wine.data"):
     A = np.loadtxt(path,delimiter=",")
@@ -21,8 +21,19 @@ wine_data, wine_labels = wine()
 
 #%%
 
-pnmf = ProjectiveNMF(wine_data, 15)
+pnmf = ProjectiveNMF(wine_data, 3)
 
 result = pnmf.predict()
 
 pl.plot(result.convgraph)
+
+#%%
+
+w = result.matrices[0]
+pl.imshow(w)
+
+assigned_labels = np.array(np.argmax(w, axis=1))[:,0]
+
+#%% Calculate NMI
+
+normalized_mutual_info_score(assigned_labels, wine_labels)
