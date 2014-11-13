899aa48b13d43abd3e971a98abec72ea55089fb2
Caner Turkmen <turkmen.ac@gmail.com>
2014-11-13 15:10:27 +0200

    working version --optimizing NSC

---
+++ applications/msdgenre.py
@@ -0,0 +1,50 @@
+"""
+Module for performing clustering on the Million Song Dataset - Genre data, as presented in
+http://labrosa.ee.columbia.edu/millionsong/blog/11-2-28-deriving-genre-dataset
+
+Author: Caner Turkmen <caner.turkmen@boun.edu.tr>
+
+"""
+
+import os,sys
+sys.path.append("/Users/Caner/code/nmflib/")
+
+from nmf import NMF
+from projective import ProjectiveNMF
+from nscsparse import NSpecSparse
+from scipy import misc
+import pylab as pl
+import numpy as np
+import pandas as pd
+from sklearn.metrics import normalized_mutual_info_score
+from sklearn.preprocessing import LabelEncoder, scale, normalize, MinMaxScaler
+
+MSD_FILE_PATH = "/Users/Caner/code/nmflib/data/msd_genre/"
+
+
+#%% import the MSD Genre Dataset
+def msd():
+
+    A1 = pd.read_csv(MSD_FILE_PATH + "msd1.txt")
+    A2 = pd.read_csv(MSD_FILE_PATH + "msd2.txt")
+    A = pd.concat((A1, A2))
+    data = A.ix[:, [5,7,8] + range(10,34)]
+    
+    le = LabelEncoder()
+    labels = le.fit_transform(A.ix[:,0])
+    return np.matrix(data), labels 
+    
+    # return np.vstack((A1, A2))
+
+X, y = msd()
+#MinMaxScaler().fit_transform(X)
+
+np.save("/Users/Caner/code/nmflib/data/msd_genre/Xarr", X)
+np.save("/Users/Caner/code/nmflib/data/msd_genre/yarr", y)
+
+#%% 
+
+nsc = ProjectiveNMF(X, 10)
+
+nsc.predict()
+
--- nscsparse.py
+++ nscsparse.py
@@ -1,11 +1,15 @@
 # coding=utf-8
+"""
+Sparse implementation of Nonnegative Spectral Clustering
+"""
 
 from basenmf import BaseNMF, NMFResult
 from scipy.spatial.distance import pdist, squareform
 import numpy as np
 from sklearn.neighbors import kneighbors_graph
-
-class NSpecClus(BaseNMF):
+from scipy.sparse import csr_matrix, diags
+#%%
+class NSpecSparse(BaseNMF):
     """
     An implementation of Non-negative Spectral Clustering, as presented in
 
@@ -13,58 +17,32 @@ class NSpecClus(BaseNMF):
     Spectral Clustering, Graph Matching, and Clique Finding.” In Eighth IEEE International Conference on
     Data Mining, 2008. ICDM ’08, 183–92, 2008. doi:10.1109/ICDM.2008.130.
 
-    The class initializer DOES NOT take an affinity matrix, but rather takes the original data matrix, computes
-    the affinity matrix via a Gaussian kernel, and performs clustering on that front.
+    This implementation of the NSpecClus, also presented in this module's nmflib.nsc.NSpecClus is for K-Nearest Neighbor
+    graphs of records, with scipy.sparse implementation.
 
     """
 
     def __init__(self, X, k, **kwargs):
         """
-
         :param X: The data matrix, **must** be a data matrix of shape (n_samples, n_features)
         :param k: number of clusters
         :return:
         """
+
         BaseNMF.__init__(self, X, k, **kwargs)
-
-        _affinity = kwargs.get("affinity")
-        if _affinity:
-            if _affinity == "nn":
-                self._affinity = "nn"
-            elif _affinity == "gaussian":
-                # will compute the Gaussian kernel of euclidean distance
-                self._affinity = "gaussian"
-            else:
-                raise Exception("Unrecognized affinity parameter %s given" % _affinity)
-        else:
-            self._affinity = "nn"
-
-        # the gamma parameter for Gaussian kernel, default 1.
+        # the gamma parameter for Gaussian kernel, default .005
         self._gamma = kwargs.get("gamma") or .005
 
 
     def predict(self):
 
-        if self._affinity == "gaussian":
-            # first convert the data matrix to pairwise distances
-            dist_matrix = squareform(pdist(self.X))
-            # then compute the affinity matrix using Gaussian kernel of the Euclidean distance
-            self.V = np.exp(-self._gamma * dist_matrix ** 2)
-
-        else:
-            # calculate the K-NN graph of the matrix
-            self.V = kneighbors_graph(self.X, n_neighbors=20)
-
-        if self._affinity == "nn":
-            # TODO: must implement sparse version as well
-            self.V = self.V.todense()
-        else:
-            self.V = np.matrix(self.V)
+        self.V = kneighbors_graph(self.X, n_neighbors=20)
 
         m, n = self.V.shape
+
         dd = np.array(self.V.sum(1))[:,0]
-        D = np.matrix(np.diag(dd))
-        H = np.matrix(np.random.rand(m, self.k))
+        D = diags(dd, format="csr")
+        H = csr_matrix(np.matrix(np.random.rand(m, self.k)))
 
         convgraph = np.zeros(self.maxiter / 10)
         distold = 0.
@@ -79,7 +57,7 @@ class NSpecClus(BaseNMF):
 
             # every 10 iterations, check convergence
             if i % 10 == 0:
-                
+
                 dist = alpha.trace()
                 print dist
                 convgraph[i/10] = dist
