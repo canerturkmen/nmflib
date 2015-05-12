b42c90b92ca56e327e5be16d5525a832d0935d1c
Caner Turkmen <turkmen.ac@gmail.com>
2015-05-12 12:52:02 +0300

    cleaning commit

--- msdgenre_explore.py
+++ msdgenre_explore.py
@@ -30,7 +30,7 @@ y = np.load(MSD_FILE_PATH + "yarr.npy")
 def draw_all_vars(X):
     """
     Draw a histogram of all the variables in the matrix X
-    
+
     :param X: input matrix
     :type X: numpy.ndarray array-like
     """
@@ -54,4 +54,3 @@ X[:,0] = -X[:,0]
 XX_MinMax = MinMaxScaler().fit_transform(X)
 
 #draw_all_vars(XX_MinMax)
-
