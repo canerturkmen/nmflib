28153b52bdc625133d5a58edaeb9247e8f3d8448
Caner Turkmen <turkmen.ac@gmail.com>
2015-05-14 12:53:35 +0300

    Sparse NMF with NNLS scipy working

--- solvers/activeset.py
+++
@@ -1,9 +0,0 @@
-"""
-
-Solve the Nonnegativity Constrained Least Squares problem via the active set method.
-
-"""
-
-
-def nnls_activeset(A, B):
-    pass
--- sparsenmf.py
+++ sparsenmf.py
@@ -1,22 +1,31 @@
 
-from .nmf import BaseNMF
+from .nmf import BaseNMF, NMFResult
+from .utils import frobenius
 from scipy.optimize import nnls
 from scipy.sparse import issparse
+import numpy as np
 
 class SparseNMF(BaseNMF):
     """
 
-    This class solves the Sparse-NMF formulation by Alternating Least Squares (NLS).
-
-    For solving the optimization problem, it relies on SciPy's NNLS solver.
 
     """
 
     def __init__(self, X, k, **kwargs):
         """
+        Initialize the Sparse-NMF class, an implementation of the cost function formulation given by
+        Kim and Park.
+
+        ```
+        SparseNMF(X, k, eta=1, beta=1, solver="nnls")
+        ```
+
+        .. math::
+            \min_{W,H} \frac{1}{2} \left[ ||A - WH^T ||_F^2 + \eta ||W||_F^2 + \beta \sum_{j=1}^{n} ||H(j,:)||_1^2  \right] s.t. W,H > 0
 
         :param X: The data matrix, **must** be a data matrix of shape (n_samples, n_features)
         :param k: number of clusters
+
         :return:
 
         """
@@ -35,7 +44,7 @@ class SparseNMF(BaseNMF):
         else:
             raise ValueError("Unrecognized solver given")
 
-    def predict():
+    def predict(self):
 
         m, n = self.X.shape
 
@@ -45,20 +54,67 @@ class SparseNMF(BaseNMF):
         else:
             X = self.X
 
-        pdist = sys.maxint #very large number
+        pdist = 1e10 #very large number
 
         # Initialize W and H with random values
         W = np.random.rand(m, self.k)
         H = np.random.rand(self.k, n)
 
-        for i in xrange(m):
-            W[i, :] = nnls(H, self.X[i, :])
-        for j in xrange(m):
-            H[:, j] = nnls()
+        dist = 0
+        converged = False
+        convgraph = np.zeros(self.maxiter / 10)
+
+        for i in xrange(self.maxiter):
+
+            W, H = self.solver(W, H)
+
+            if i % 10 == 0:
+                dist = frobenius(X, np.dot(W,H))
+                convgraph[i/10] = dist
+
+                if pdist - dist < self.stopconv:
+                    converged = True
+                    break
+
+                pdist = dist
+
+        # normalize W,H before returning
+        norms = np.linalg.norm(W, 2, axis=0)
+        norm_gt_0 = norms > 0
+        W[:, norm_gt_0] /= norms[norm_gt_0]
+        H[norm_gt_0, :] = ((H[norm_gt_0, :].T) * norms[norm_gt_0]).T
+
+        return NMFResult((W,H), convgraph, dist, converged)
 
 
-    def _update_scipy_nnls(W, H):
-        pass
+    def _update_scipy_nnls(self, W, H):
+        """
+        Run the update step with regularized cost function, and Nonnegative Least Squares
+        provided by SciPy (activeset variant)
+
+        :param W: the left factorizing matrix
+        :param H: the right factorizing matrix
+        :type W: numpy.ndarray
+        :type H: numpy.ndarray
+        :returns: two-tuple (W,H) with new matrices
+        """
+
+        # 'augmented' data matrix with vector of zeros
+        Xaug = np.r_[self.X, np.zeros((1,H.shape[1]))]
+
+        # 'augmented' left factorizing matrix with vector of ones
+        Waug = np.r_[W, np.sqrt(self.beta)*np.ones((1,5))]
+
+        Htaug = np.r_[H.T, np.sqrt(self.eta) * np.eye(H.shape[0])]
+        Xaugkm = np.r_[self.X.T, np.zeros(W.T.shape)]
+
+        for i in xrange(W.shape[0]):
+            W[i, :] = nnls(Htaug, Xaugkm[:, i])[0]
+
+        for j in xrange(H.shape[1]):
+            H[:, j] = nnls(Waug, Xaug[:, j])[0]
+
+        return (W,H)
 
     def _update_scipy_lstsq(W, H):
         pass
